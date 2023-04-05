8ed301661e26805c5821e070a59ff6e6c944cd37
giovannilucas <giovannilramos55@gmail.com>
2023-04-05 10:39:36 -0300

    feat: add keycloack

---
+++ api-gateway/src/main/java/br/com/quaz/apigateway/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package br.com.quaz.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(final ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity.cors().and().csrf().disable()
+                .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
+                        .pathMatchers("/eureka/**")
+                        .permitAll()
+                        .anyExchange()
+                        .authenticated()
+                ).oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+        return serverHttpSecurity.build();
+    }
+}
